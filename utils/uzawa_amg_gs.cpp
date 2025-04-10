e600a8238b78ff69ef25f9d5bfe25b1b0346cd72
Kirill Terekhov <kirill.terehov@gmail.com>
2025-04-10 18:32:36 +0300

    Measure time and memory

--- utils/uzawa_amg_gs.cpp
+++ utils/uzawa_amg_gs.cpp
@@ -3,6 +3,7 @@
 #include "amg_ruge_stuben.h"
 #include "gauss_seidel.h"
 #include "ilduc.h"
+#include "get_time.h"
 
 int main(int argc, char ** argv)
 {
@@ -47,8 +48,20 @@ int main(int argc, char ** argv)
 		std::cout << "Set input block size: " << N << std::endl;
 		Solver.GetParameters().Set<int>("B_block_end", N);
 		Solver.GetParameters().Print();
-		if( Solver.Setup(A) && Solver.Solve(b,x) )
+		bool success = true;
+		double t1, t2, t3, t4;
+		t1 = get_time();
+		success &= Solver.Setup(A);
+		t2 = get_time();
+		t3 = get_time();
+		success &= Solver.Solve(b, x);
+		t4 = get_time();
+		if (success)
 		{
+			std::cout << "Time setup " << t2 - t1 << " sec iterations " << t4 - t3 << " sec solve " << t4 - t1 << " sec" << std::endl;
+			std::cout << "Solver consumed: " << Solver.Bytes() / 1024 << " KB" << std::endl;
+			std::cout << "Matrix consumed: " << A.Bytes() / 1024 << " KB" << std::endl;
+			std::cout << "Vector consumed: " << get_bytes(b) / 1024 << " KB" << std::endl;
 			std::cout << "Final residual " << Resid(A,b,x) << std::endl;
 			SaveVector(std::string("solution"),x);
 			return 0;
